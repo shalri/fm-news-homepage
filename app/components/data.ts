0ec3b6983de8307b81671732eb15483a520b6103
shalri <fsaonlinework777@gmail.com>
2024-05-27 23:45:24 +0800

    Merge challenge assets and data

---
+++ app/components/data.ts
@@ -0,0 +1,57 @@
+export const navLinks = [
+  {
+    page: "Home",
+    pagUrl: "#",
+  },
+  {
+    page: "New",
+    pagUrl: "#",
+  },
+  {
+    page: "Popular",
+    pagUrl: "#",
+  },
+  {
+    page: "Trending",
+    pagUrl: "#",
+  },
+  {
+    page: "Categories",
+    pagUrl: "#",
+  },
+];
+
+export const newArticles = [
+  {
+    articleTitle: "Hydrogen VS Electric Cars",
+    articleSubhead: "Will hydrogen-fueled cars ever catch up to EVs?",
+  },
+  {
+    articleTitle: "The Downsides of AI Artistry",
+    articleSubhead:
+      "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    articleTitle: "Is VC Funding Drying Up?",
+    articleSubhead:
+      "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+  },
+];
+
+export const popularArticles = [
+  {
+    rank: "01",
+    title: "Reviving Retro PCs",
+    subhead: "What happens when old PCs are given modern upgrades?",
+  },
+  {
+    rank: "02",
+    title: "Top 10 Laptops of 2022",
+    subhead: "Our best picks for various needs and budgets.",
+  },
+  {
+    rank: "03",
+    title: "The Growth of Gaming",
+    subhead: "How the pandemic has sparked fresh opportunities.",
+  },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        // primarty
+        "nh-soft-orange": "hsl(35, 77%, 62%)",
+        "nh-soft-red": "hsl(5, 85%, 63%)",
+        // Neutral
+        "nh-off-white": "hsl(36, 100%, 99%)",
+        "grayish-blue": "hsl(233, 8%, 79%)",
+        "dark.grayish-blue": "hsl(236, 13%, 42%)",
+        "very.dark.blue": "hsl(240, 100%, 5%)",
+      },
+      fontFamily: {
+        "inter": ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        normal: "400",
+        semibold: "700",
+        bold: "800",
+      },
+      fontSize: {
+        body: "15px"
+      },
+      screen: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
       backgroundImage: {
         // "sample-bg": "/fm-news-homepage"
       },
