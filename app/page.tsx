2892b8c55ae30b47557a7a41a67389aa7682d178
shalri <fsaonlinework777@gmail.com>
2024-05-27 22:32:14 +0800

    Init commit using Tsugini template

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import SandBox from "./components/Sandbox";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex flex-grow flex-col items-center justify-center p-24">
+        <SandBox />
+      </main>
+      <Footer />
+    </>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        // "sample-bg": "/fm-news-homepage"
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
